eb475ea3cde240a5c6073ba5d9a01e9b275de080
Emanuele.Goldin@student.uibk.ac.at <Emanuele.Goldin@student.uibk.ac.at>
2025-01-19 17:12:18 +0100

    Added movement related components

---
+++ scenes/components/gravity/gravity_component.gd
@@ -0,0 +1,12 @@
+class_name GravityComponent
+extends Node
+
+@export_subgroup("Settings")
+@export var gravity: float = 1000.0
+
+var is_falling: bool = false
+
+func handle_gravity(body: CharacterBody2D, delta:float):
+	if not body.is_on_floor():
+		body.velocity.y += gravity * delta
+	is_falling = body.velocity.y > 0 and not body.is_on_floor()
---
+++ scenes/components/move/movement_component.gd
@@ -0,0 +1,17 @@
+class_name MovementComponent
+extends Node
+
+@export_subgroup("Settings")
+@export var speed: float = 100.0
+@export var ground_accel: float = 6.0
+@export var ground_decel: float = 8.0
+@export var air_accel: float = 10.0
+@export var air_decel: float = 3.0
+
+func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
+	var velocity_change_speed: float = 0.0
+	if body.is_on_floor():
+		velocity_change_speed = ground_accel if direction != 0 else ground_decel
+	else:
+		velocity_change_speed = air_accel if direction != 0 else air_decel
+	body.velocity.x = move_toward(body.velocity.x, direction * speed, velocity_change_speed)
