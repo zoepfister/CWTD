0f17a485a9a6e0d24b82ce9a002a8bef93e4185b
Zoe Pfister <zoe@pfister.pw>
2025-01-26 00:48:19 +0100

    add pause menu

--- scenes/components/input/input_component.gd
+++ scenes/components/input/input_component.gd
@@ -3,12 +3,21 @@ extends Node
 
 var input_horizontal: float = 0.0
 
+signal check_escape
+
 func _process(delta: float) -> void:
 	input_horizontal = Input.get_axis("move_left","move_right")		# input_horizontal will be -1 when we move left,
 																	# 1 when move right, and 0 when we don't move
-	
+	get_escape_input()
+
 func get_jump_input() -> bool:
 	return Input.is_action_just_pressed("jump")
 	
 func get_jump_released() -> bool:
 	return Input.is_action_just_released("jump")
+
+func get_escape_input() -> bool:
+	var escape_pressed = Input.is_action_just_pressed("escape")
+	if escape_pressed:
+		check_escape.emit()
+	return escape_pressed
--- scenes/menus/pause_menu.gd
+++ scenes/menus/pause_menu.gd
@@ -4,20 +4,23 @@ extends Control
 @onready var tinamy_shadow: PlayerSprite = $PlayerFakeShadow
 @onready var sound_manager = $SoundManager
 
-@export var start_scene = "res://scenes/Game.tscn"
-
-func _ready() -> void:
-	tinamy.play_animation("run")
-	tinamy_shadow.play_animation("run")
-	#sound_manager.start_background_music()
+#@export var start_scene = "res://scenes/Game.tscn"
+#
+#func _ready() -> void:
+	#tinamy.play_animation("run")
+	#tinamy_shadow.play_animation("run")
+	##sound_manager.start_background_music()
 
 func _on_pause_pressed() -> void:
 	get_tree().paused = true
 	show()
 	
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file(start_scene)
 
 func _on_resume_pressed() -> void:
 	hide()
 	get_tree().paused = false
+
+
+func _on_restart_pressed() -> void:
+	_on_resume_pressed()
+	get_tree().reload_current_scene()
