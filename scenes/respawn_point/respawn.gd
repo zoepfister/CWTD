46e968129d1d2aea7d4d2008081eeccd7f12837f
Emanuele.Goldin@student.uibk.ac.at <Emanuele.Goldin@student.uibk.ac.at>
2025-01-27 20:38:12 +0100

    removed unused player reference

--- scenes/respawn_point/respawn.gd
+++ scenes/respawn_point/respawn.gd
@@ -1,14 +1,10 @@
 class_name Respawn
 extends AnimatedSprite2D
 
-@onready var player: Player = get_tree().get_first_node_in_group("player")
-
 signal respawn_animation_finished
 
-var animaiton_finished: Callable = Callable(self, "_on_animation_finished") 
-
 func _ready() -> void:
-	animation_finished.connect(animaiton_finished)
+	animation_finished.connect(_on_animation_finished)
 	
 func play_respawn_animation() -> void:
 	play("respawn")
