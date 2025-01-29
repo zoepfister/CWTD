bc98c02156ca7c60dd4be6ac973b1c545d668469
Zoe Pfister <zoe@pfister.pw>
2025-01-29 21:11:31 +0100

    pause menu fixes

--- scenes/menus/pause_menu.gd
+++ scenes/menus/pause_menu.gd
@@ -11,10 +11,15 @@ extends Control
 	#tinamy_shadow.play_animation("run")
 	##sound_manager.start_background_music()
 
+func _ready() -> void:
+	$"../../Player/Components/InputComponent".connect("check_escape", _on_pause_pressed)
+	tinamy.play_animation("idle")
+	tinamy_shadow.play_animation("idle")
+
+
 func _on_pause_pressed() -> void:
 	get_tree().paused = true
 	show()
-	
 
 func _on_resume_pressed() -> void:
 	hide()
@@ -23,4 +28,5 @@ func _on_resume_pressed() -> void:
 
 func _on_restart_pressed() -> void:
 	_on_resume_pressed()
+	sound_manager.stop_background_music()
 	get_tree().reload_current_scene()
